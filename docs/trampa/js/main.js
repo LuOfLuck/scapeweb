f8ed97468143d59bdb7c54d12fb0c90dd5332bd9
Lu <you@example.com>
2021-11-27 11:29:40 -0300

    primer scape web beta 1.0 hecho :D

---
+++ docs/trampa/js/main.js
@@ -0,0 +1,30 @@
+const popou = document.getElementById("id_popou");
+const button_exit = document.getElementById("exit__popou")
+const abrir_popou = document.getElementById("abrir_popou")
+const verificar = document.getElementById("verificar")
+const clave = document.getElementById("id_clave")
+const move =document.getElementById('id_move')
+const capa = document.getElementById("id_capa")
+var valor = 0
+
+button_exit.onclick = () =>{
+	popou.classList.remove("flex")
+}
+function abrirPopou(){
+	popou.classList.add("flex")
+}
+
+
+verificar.onclick = () => {
+	if(clave.value == "callefalsa123"){
+		abrir_popou.onclick = null
+		alert("clave correcta :)")
+		button_exit.onclick()
+		move.style.width = "30px"
+	}
+}
+
+move.addEventListener("mousemove", ()=>{
+	valor += 15
+	capa.style.transform = `translateX(${valor}px)`
+})
