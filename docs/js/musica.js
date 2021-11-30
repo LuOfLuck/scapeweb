7dc3ee8a582b0b57af2a3e95d08ba71814bb4a5b
Lu <you@example.com>
2021-11-29 22:47:33 -0300

    beta 1.1 musica agregada

---
+++ docs/js/musica.js
@@ -0,0 +1,57 @@
+if(window.location.host == "www.scapeweb.online"){
+	var ruta = window.location.host
+}else{
+	var ruta = "file:///C:/Users/Lucas/Desktop/divertido/docs/"
+}
+document.write(`
+	<div class="audio" style="z-index:1000;">
+		<audio id="audio_clic" src="${ruta}/musica/clic_normal.mp3"></audio>
+		<audio id="audio_clic_bueno" src="${ruta}/musica/clic_bueno.mp3"></audio>
+		<audio id="musica" src="${ruta}/musica/index.mp3"></audio>
+		<div id="id_audio_estado">
+			<i id="icon_musica" class="fas fa-play"></i>
+			<i id="icon_efectos" class="fas fa-volume-up"></i>
+		</div>
+		
+	</div>
+`)
+console.log(window.location.href, ruta)
+const musica = document.getElementById("musica");
+const iconMusica = document.getElementById("icon_musica");
+const iconEfectos = document.getElementById("icon_efectos");
+const audioClic = document.getElementById("audio_clic")
+const audioClicBueno = document.getElementById("audio_clic_bueno")
+
+
+
+document.addEventListener("click", (e)=>{
+	console.log(e.target == iconMusica)
+	if(e.target == iconMusica){
+		audioClicBueno.play()
+	}else{
+		audioClic.play()
+	}	
+})
+
+const pausar_o_reproducir = () =>{
+	if (iconMusica.classList.contains("fa-pause")){
+		iconMusica.classList.replace("fa-pause", "fa-play");
+		musica.pause()
+	}else{
+		iconMusica.classList.replace("fa-play","fa-pause");
+		musica.play()
+	}
+}
+const efectosSonds = () => {
+	if (iconEfectos.classList.contains("fa-volume-mute")){
+		iconEfectos.classList.replace("fa-volume-mute", "fa-volume-up");
+		audioClic.volume = 1
+		audioClicBueno.volume = 1
+	}else{
+		iconEfectos.classList.replace("fa-volume-up","fa-volume-mute");
+		audioClic.volume = 0
+		audioClicBueno.volume = 0
+	}
+}
+iconMusica.addEventListener("click", pausar_o_reproducir);
+iconEfectos.addEventListener("click", efectosSonds);
